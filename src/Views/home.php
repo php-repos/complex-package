4779cfdc7cde1c746e30428a41381c747cc8c8d1
Morteza Poussaneh <49806042+MortezaPoussaneh@users.noreply.github.com>
2022-10-10 09:54:11 +0200

    add function usages to allow import classes

--- src/Views/home.php
+++ src/Views/home.php
@@ -4,6 +4,8 @@ use Saeghe\ComplexPackage\Models\User;
 use Saeghe\SimplePackage\SimpleClass;
 use Saeghe\ComplexPackage\Helpers;
 
+$user = new User();
+$description = SimpleClass::description();
 ?>
 
-<h1>Hello World!</h1>
+<h1>Hello World! <?php Helper\output(); ?></h1>
